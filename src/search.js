88b7b8501f7d46cbb6ca4b26829fa7f724ecdd79
v_hxin <v_hxin@digitalgd.com.cn>
2021-04-23 20:22:21 +0800

    [+]load assets: file-loader and url-loader

--- src/search.js
+++ src/search.js
@@ -1,11 +1,17 @@
 "use strit";
 import "./styles/index.scss";
+import LPImage from "./assets/5b4e55e250536a1b1d619bef1998b279.png";
 import React from "react";
 import ReactDOM from "react-dom";
 
 export class Search extends React.Component {
   render() {
-    return <div className="search-text">This is the Search Page</div>;
+    return (
+      <div className="search-text">
+        This is the Search Page
+        <img src={LPImage}></img>
+      </div>
+    );
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,23 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      // prior webpack5
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: "url-loader",
+      //       options: { limit: 8192 },
+      //     },
+      //   ],
+      //   // use: "file-loader",
+      //   type: "javascript/auto", // for webpack5
+      // },
+
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: "asset",
+      },
     ],
   },
 };
