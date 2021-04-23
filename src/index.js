8d729eb9108c60f5feb3198cd5767c5c2f7c8f50
v_hxin <v_hxin@digitalgd.com.cn>
2021-04-23 17:11:58 +0800

    [+]initialize webpack-learning

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { hello } from './helloworld'
+
+function main() {
+  console.log(hello());
+}
+main()
---
+++ src/search.js
@@ -0,0 +1,11 @@
+"use strit";
+import React from "react";
+import ReactDOM from "react-dom";
+
+export class Search extends React.Component {
+  render() {
+    return <div>This is the Search Page</div>;
+  }
+}
+
+ReactDOM.render(<Search />, document.getElementById("root"));
